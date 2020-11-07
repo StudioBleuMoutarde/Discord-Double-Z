2d5343b1d15fa1fae4d04586ec9c121d635a6575
Frédéric Llorca <frederic@mbp-de-frederic.home>
2020-11-07 16:44:20 +0100

    Décompte de départ

--- src/classes/game.js
+++ src/classes/game.js
@@ -10,17 +10,28 @@ module.exports = class Game {
     this.activeQuestionIndex = 0;
   }
 
-  async start() {
+  start() {
     this.textChannel.send('La partie va commencer !');
 
     // Recherche des joueurs dans le channel vocal
     this.voiceChannel.members.forEach((member) => {
       // Ne pas s'enregistrer soi
-      if (member.userID !== process.env.ADMIN_ID) {
+      // if (member.userID !== process.env.ADMIN_ID) {
         this.players.push(new Player(member));
         this.textChannel.send(`${member.displayName} enregistré`);
-      }
+      // }
     });
+
+    // Décompte de début de partie
+    const COUNTDOWN_MS = 4000;
+    let countdown = COUNTDOWN_MS / 1000;
+    const countdownInterval = setInterval(() => {
+      this.textChannel.send(`La partie commence dans ${countdown - 1}`);
+      countdown -= 1;
+    }, 1000);
+    setTimeout(() => {
+      clearInterval(countdownInterval);
+    }, COUNTDOWN_MS);
   };
 
   async registerPlayers() {
