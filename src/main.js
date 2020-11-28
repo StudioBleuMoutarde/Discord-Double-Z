5af2f30f2ce5406818edb485c60d5df11b388cbc
Frédéric Llorca <frederic@mbp-de-frederic.home>
2020-11-28 16:22:32 +0100

    channels selon si dev ou prod

--- src/main.js
+++ src/main.js
@@ -6,13 +6,15 @@ const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 const Game = require('./classes/game');
 const game = new Game();
 
+const channelName = process.env.ENVIRONMENT === 'development' ? /test/i : /plateau/i;
+
 // Réception d'un message
 client.on('message', (message) => {
   // On ne prend pas en compte les messages de bot
   if(message.author.bot) return;
 
   // Seulement les messages du channel text Plateau
-  if (message.channel.type !== 'text' && !/plateau/i.test(message.channel.name)) return;
+  if (message.channel.type !== 'text' && !channelName.test(message.channel.name)) return;
 
   // Seulement les messages de l'admin
   if (message.author.id === process.env.ADMIN_ID) {
@@ -50,16 +52,16 @@ client.on('ready', () => {
   // Recherche des références des channels "Plateau"
   client.channels.cache.forEach((chan) => {
     // regex pour le nom du channel "Plateau"
-    const isNamePlateau = /plateau/i.test(chan.name);
+    const isNamePlateau = channelName.test(chan.name);
 
     if (chan.type === 'voice' && isNamePlateau) {
       game.voiceChannel = chan;
-      console.log('- voice channel "Plateau" found');
+      console.log(`- voice channel "${channelName}" found`);
     }
 
     if (chan.type === 'text' && isNamePlateau) {
       game.textChannel = chan;
-      console.log('- text channel "Plateau" found');
+      console.log(`- text channel "${channelName}" found`);
     }
   });
 });
